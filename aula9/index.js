1a9f051eb827f745b74fa93643c95e05e5d603f3
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-08-17 23:40:02 -0300

    criei aula 9

--- aula9/index.js
+++ aula9/index.js
@@ -1,7 +1,9 @@
-// EcmaScript 2015 (ES6)
+// EcmaScript 2015 (ES6) JavaScript
 
 
-var nome = 'rodrigo';
-var nome = 'sena';
+var nome = 'Rodrigo';
+var nome = 'Sena';
 
 console.log(nome);
+
+// Var pode ser redeclarada facilmente, já o let corrige isso, por isso sempre use o let
