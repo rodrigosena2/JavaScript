a819571f825e60bbd48177cd19751db7d58e1cf5
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-08-18 00:19:51 -0300

    criei aula 10

---
+++ aula10/index.js
@@ -0,0 +1,24 @@
+/* 
+* Aritméticos 
+* + Adição / Concatenação
+* - Subtração
+* /  Divisão
+* *  Multiplicação
+* ** Potenciação
+* % Resto da divisão
+ ordem de execução
+ (), **, * / %, + -.
+
+ Incremento = ++
+ Decremento = --
+*/
+
+/* const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log((num1 + num2) * num3); */
+
+
+let contador = 2;
+contador **= 10;
+console.log(contador);
---
+++ aula10/index2.js
@@ -0,0 +1,5 @@
+// NaN - Not a number, parseInt (Inteiro), parseFloat(decimais), Number (se vira para descobrir que tipo de número é)
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);
+console.log(typeof num2); 
