7ef673ec2b23a55c15a5ba546de144944fb0b9a8
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-08-20 02:27:45 -0300

    Create index.js

---
+++ aula16/index.js
@@ -0,0 +1,42 @@
+//               0       1        2          3      4
+let alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+alunos = 123;
+
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array)
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -1));
+
+
+// console.log(alunos[50]);
+
+
+// delete alunos[1];
+// console.log(alunos);
+
+
+// const removido = alunos.shift();
+// console.log(alunos);
+// console.log(removido);
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos[3] = 'Luiza';
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
