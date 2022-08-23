a352cc74c7123933158401a8c3f020d418cb4e4c
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-08-23 20:19:12 -0300

    Update index.js

---
+++ aula17/index.js
@@ -0,0 +1,27 @@
+const raiz = function(n) {
+    return n ** 0.5;
+}
+    
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+
+/* function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma(5, 10);
+console.log(resultado); */
+
+
+
+/*function saudacao(nome) {
+    return `Bom dia ${nome}!`;
+}
+
+const variavel = saudacao('Rodrigo');
+console.log(variavel);*/
