036ebd6c17a5696a749d10c3b33c0cdfd339aeea
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-08-23 21:26:26 -0300

    Create index.js

---
+++ aula19/index.js
@@ -0,0 +1,17 @@
+/*
+Primitivos (imutáveis) - String, number, boolean, undefined, 
+null(bigint, symbol) - Valores copiados  
+
+Referência (mutável) - array, object, function - Passados por referência
+
+*/
+
+const a = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+};
+const b = a;
+
+b.nome = 'João';
+console.log(a);
+console.log(b);
