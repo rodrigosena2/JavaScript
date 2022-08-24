eab647141364f801c64d77ce4d46ff2fb6b08e00
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-08-24 19:04:08 -0300

    Create index.js

---
+++ aula21/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) não é utilizado
+=== igualdade estrita (valor e tipo) o mais utilizado
+!= diferente (valor) não é utilizado
+!== diferente estrito (valor e tipo) o mais utilizado
+*/
+const num1 = 10;
+const num2 = '10';
+
+const comp = num1 !== num2;
+console.log(comp);
