7aa28f25af5011d8502c1ad3cccd9150b927a194
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-09-24 19:49:11 -0300

    Update index.js

--- aula23/index.js
+++ aula23/index.js
@@ -1,18 +1,5 @@
-/*
-&& -> false && true -> false "o valor mesmo"
-|| -> true || false -> vai retornar "o valor verdadeiro"
-
-FALSY
-* false
-* 0
-* '' "" ``
-* null / undefined
-* NaN
+/* 
+&& -> Todas as expressôes precisam ser verdadeiras para dar true -> false && true -> false " o valor mesmo" 
+|| -> Apenas uma expressão precisa ser verdadeira para dar true -> 
 */
-const a = 0;
-const b = null;
-const c = false;
-const d = false; 
-const e = NaN;
-
-console.log(a || b || 'Joãozinho' || c || d || e);
+console.log('Luiz Otávio' && 0 && 'Maria')
