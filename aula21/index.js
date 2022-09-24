81ffcd4c21844dca804dd082544f79f361b6ffb3
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-09-24 19:08:14 -0300

    Update index.js

--- aula21/index.js
+++ aula21/index.js
@@ -9,5 +9,7 @@ Operadores de comparação
 != diferente (valor) *********
 !== diferente estrito (valor e tipo)
 */
-const comp = 9 <= 9;
+const num1 = 10; // number
+const num2 = 10; // string
+const comp = num1 !== num2;
 console.log(comp);
