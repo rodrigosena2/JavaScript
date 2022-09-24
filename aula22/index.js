abfcc7385a4747c2200c68b6550b151f11eb167f
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-09-24 19:09:21 -0300

    Update index.js

--- aula22/index.js
+++ aula22/index.js
@@ -1,16 +1,6 @@
 /*
 Operadores Lógicos
-&& -> AND -> E - > Todas as expressões precisam ser verdadeiras para retornar true
-|| -> OR -> OU -> Apenas uma expressão precisa ser verdadeira pra retornar true
+&& -> AND -> E
+zz -> OR -> OU
 ! -> NOT -> NÃO
 */
-// const expressaoAnd = true && true && true && false;
-// const expressaoOr = true || false || true || true;
-
-//const usuario = 'Luiz'; // form usuário digitou
-//const senha = '123456'; // form usuário digitou
-
-//                       True                 False
-// const vaiLogar = usuario === 'Luiz' && senha === '12345';
-
-console.log(!!true);
