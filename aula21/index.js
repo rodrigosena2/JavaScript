c6865267406bed46518897f018e28b094e4f9461
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-09-24 18:49:53 -0300

    Update index.js

--- aula21/index.js
+++ aula21/index.js
@@ -4,13 +4,8 @@ Operadores de comparação
 >= maior que ou igual a
 < menor que
 <= menor que ou igual a
-== igualdade (valor) não é utilizado
-=== igualdade estrita (valor e tipo) o mais utilizado
-!= diferente (valor) não é utilizado
-!== diferente estrito (valor e tipo) o mais utilizado
+== igualdade 
+=== igualdade estrita
+!= diferente
+!== diferente estrito
 */
-const num1 = 10;
-const num2 = '10';
-
-const comp = num1 !== num2;
-console.log(comp);
