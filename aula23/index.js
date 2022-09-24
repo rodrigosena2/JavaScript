7a7dd8092e0e19f155777b0833b576ff35cb8f9a
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-09-24 20:04:12 -0300

    Update index.js

--- aula23/index.js
+++ aula23/index.js
@@ -10,4 +10,10 @@ null / undefined
 NaN
 
 */
-console.log(0 || false || null | 'Rodrigo Sena' || true);
+const a = 0;
+const b = null;
+const c = false; // Aqui
+const d = false;
+const e = NaN;
+
+console.log( a || b || c || d || e);
