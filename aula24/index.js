5bc44f28333437997a48fdabb7f37e8e47f799a3
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-09-30 17:09:48 -0300

    Update index.js

--- aula24/index.js
+++ aula24/index.js
@@ -1,32 +1,22 @@
 /*
 Entre 0 - 11 - Bom dia
 Entre 12 - 17 - Boa tarde
-entre 18 - 23 - Boa noite
+Entre 18 - 23 - Boa noite
 */
 
-// if pode ser usado sozinho
-// Sempre que utilizo a palavra else, preciso de um if antes
+// If pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes 
 // Eu posso ter vários else ifs na checagem
-// Só posso ter um else na checagem
-// Podemos usar condições sem else if, utilizando apenas if e else 
 
-/* const hora = 159;
+
+const hora = 15;
 
 if (hora >= 0 && hora <= 11) {
     console.log('Bom dia');
 } else if (hora >= 12 && hora <= 17) {
-    console.log('Boa tarde');
+    console.log('Boa tarde')
 } else if (hora >= 18 && hora <= 23) {
-    console.log('Boa noite');
+    console.log('Boa noite')
 } else {
-    console.log('Olá')
-}
-*/
-
-const tenhoGrana = true;
-
-if (tenhoGrana) {
-    console.log('Vou sair de casa');
-} else {
-    console.log('Não vou sair de casa');
+    console.log('Informe uma hora correta')
 }
