69c96e86050c210932a44ef8cbe1a22c1596ce16
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-10-04 07:07:03 -0300

    Update index.js

--- aula28/index.js
+++ aula28/index.js
@@ -1,33 +1,11 @@
-function zeroAEsquerda (num) {
-    return num >= 10 ? num : `0${num}`;
-}
-
-
-function formataData(data) {
-    const dia = zeroAEsquerda(data.getDate());
-    const mes = zeroAEsquerda(data.getMonth() + 1);
-    const ano = zeroAEsquerda(data.getFullYear());
-    const hora = zeroAEsquerda(data.getHours());
-    const min = zeroAEsquerda(data.getMinutes());
-    const seg = zeroAEsquerda(data.getSeconds());
-
-    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
-}
-
-const data = new Date();
-const dataBrasil = formataData(data);
-console.log(dataBrasil);
-
-
-
-
-
-/*const tresHoras = 60 * 60 * 3 * 1000;
+/* 
+const tresHoras = 60 * 60 * 3 * 1000;
 const umDia = 60 * 60 * 24 * 1000;
-const data = new Date(0 + tresHoras + umDia); // 01/01/1970 Timestamp unix ou época unix */
-// 0 - 11 Meses 0 é considerado janeiro e 11 dezembro
-// const data = new Date(2019, ); // a, m, d, h, M, s, ms
-/* const data = new Date('2019-04-20T20:20:59.100');
+const data = new Date(0 + tresHoras + umDia); // 01/01/1970 Timestamp unix ou época unix
+0-11 referente a meses, janeiro é 0 e dezembro é 11
+ */
+// const data = new Date(2019, 3, 20); // a, m, d, h, M, s, ms
+/* const data = new Date(1664877552485)
 console.log('Dia', data.getDate());
 console.log('Mês', data.getMonth() + 1); // Mês começa do zero
 console.log('Ano', data.getFullYear());
@@ -37,5 +15,24 @@ console.log('Seg', data.getSeconds());
 console.log('ms', data.getMilliseconds());
 console.log('Dia semana', data.getDay()); // 0 - Domingo, 6 - Sábado
 console.log(data.toString());
-console.log(Date.now()); 
+console.log(Date.now());
 */
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano}/ ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
