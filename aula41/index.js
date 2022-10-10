cc3a43d91cf25ca04445a31f7102cc6b2c82b161
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-10-10 09:52:24 -0300

    Create index.js

---
+++ aula41/index.js
@@ -0,0 +1,36 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+const max = (x, y) => x > y ? x : y;
+
+console.log(max(10, 2));
+
+
+
+
+/*
+function max(x, y) {
+    return x > y ? x : y;
+} */
+
+/*
+function max(x, y) {
+    if (x > y) {
+        return x;
+    }
+    return y
+}
+*/
+
+
+
+
+/*
+(function max(x, y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+console.log(max(1, 2)) */
