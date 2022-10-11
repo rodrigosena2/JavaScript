75114d872f3890ab4ae68600512cd7d18de2f542
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-10-11 08:33:34 -0300

    Create index.js

---
+++ aula45/index.js
@@ -0,0 +1,44 @@
+/*
+try {
+    // É executada quando não há erros
+    console.log(a);
+    console.log('Abri um arquivo');
+    console.log('Manipulei o arquivo e gerou erro');
+    console.log('Fechei o arquivo');
+} catch (err) {
+    // É executada quando há erros
+    console.log('Tratando o erro');
+} finally {
+    // Sempre
+    console.log('FINALLY:  Eu sempre sou executado');
+}
+*/
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date.');
+    }
+
+    if ('data') {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+
+    });
+
+}
+try {
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora(data);
+    console.log(hora);
+} catch(err) {
+    // Tratar erro
+    // console.log(err);
+} finally {
+    console.log('Tenha um bom dia.')
+}
