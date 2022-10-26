958406a8527fd465b2c8118e3aa55a469fda6ad6
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-10-26 13:13:23 -0300

    Update index.js

--- aula56/index.js
+++ aula56/index.js
@@ -1,15 +1,42 @@
+// Factory function (Função fábrica)
+// Constructor function (função construtora)
 function criaPessoa(nome, sobrenome, altura, peso) {
     return {
         nome,
         sobrenome,
-        fala(assunto) {
+
+        // Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+            console.log(valor);
+        },
+
+        fala(assunto = 'falando sobre Nada') {
             return `${this.nome} está ${assunto}.`;
         },
         altura: altura,
-        peso: peso
+        peso: peso,
+        // Getter
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
     };
 }
 
-const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
-const p2 = criaPessoa('Maria', 'Joaquina', 1.6, 50);
-console.log(p2.fala('falando sobre JS'));
+const p1 = criaPessoa('Paulo', 'Otávio', 1.8, 70);
+const p2 = criaPessoa('Carlos', 'Otávio', 1.6, 80);
+const p3 = criaPessoa('Luiz', 'Otávio', 1.5, 110);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
+
+
