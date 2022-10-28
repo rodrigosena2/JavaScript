0a16ac80cae2453831c7c4691bb72ff9a5251b61
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-10-28 10:57:11 -0300

    Create index.js

---
+++ aula62/index.js
@@ -0,0 +1,10 @@
+// Valor por referência
+// String, Objetos, Funções, Números
+//                 0         1        2         3          4
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+const nomes = ['Luiz', 'Otávio', 'Miranda'];
+const nome = nomes.join(', ');
+console.log(nome);
