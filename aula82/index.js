34e39ee543e8bd35b57946b91868efbff5a8f40f
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-11-29 13:32:01 -0300

    Update index.js

--- aula82/index.js
+++ aula82/index.js
@@ -22,3 +22,34 @@ class DispositivoEletronico {
         this.ligado = false;
     }
 }
+
+class Smartphone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        super(nome);
+
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+class Tablet extends DispositivoEletronico {
+    constructor(nome, temWifi) {
+        super(nome);
+        this.temWifi = temWifi;
+    }
+
+    ligar() {
+        console.log('Olha, você alterou o método ligar.')
+    }
+    falaOi() {
+        console.log('Oi');
+    }
+}
+
+const s1 = new Smartphone('Samsung', 'Preto', 'Galaxy S10');
+const t1 = new Tablet('iPad', true);
+t1.ligar();
+t1.ligar();
+t1.falaOi();
+
+
