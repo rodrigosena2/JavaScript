996f993f0cc57e5f2effd55f27c9742ff50308b0
Rodrigo Sena <contatorodrigosena1@gmail.com>
2022-11-29 12:58:25 -0300

    Update index.js

--- aula81/index.js
+++ aula81/index.js
@@ -1,10 +1,67 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    get nomeCompleto() {
+        return this.nome + ' ' + this.sobrenome;
+    }
+
+    set nomeCompleto(valor) {
+        valor = valor.split(' ');
+        this.nome = valor.shift();
+        this.sobrenome = valor.join(' ');
+    }
+}
+
+const p1 = new Pessoa('Luiz', 'Miranda');
+p1.nomeCompleto = 'Luiz Miranda Oliveira'
+console.log(p1.nome)
+console.log(p1.sobrenome)
+
+
+
+
+
+
+/*
+const _velocidade = Symbol('velocidade');
 class Carro {
     constructor(nome, velocidade) {
         this.nome = nome;
-        this.velocidade = 0;
+        this[_velocidade] = 0;
+    }
+
+    set velocidade(valor) {
+        console.log('SETTER');
+        if(typeof valor !== 'number') return;
+        if(valor >= 100 || valor <= 0) return;
+        this[_velocidade] = valor;
+    }
+
+    get velocidade() {
+        console.log('GETTER');
+        return this[_velocidade];
     }
 
     acelerar() {
-        this.velocidade++;
+        if(this[_velocidade] >= 100) return;
+        this[_velocidade]++;
+    }
+
+    freiar() {
+        if(this[_velocidade] <= 0) return;
+        this[_velocidade]--;
     }
 }
+
+const c1 = new Carro('Fusca');
+
+//for(let i = 0; i<= 200; i++) {
+//    c1.acelerar();
+//}
+
+c1.velocidade = 55;
+console.log(c1.velocidade);
+*/
